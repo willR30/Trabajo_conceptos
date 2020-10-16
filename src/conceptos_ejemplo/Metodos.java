ad89b55a7dbb25d3f8cc2301fd0699228002f69b
william <prueba@correo.com>
2020-10-15 22:28:03 -0600

    ejemplo completo , sin interfaz grafica POO aplicada en todo

--- src/conceptos_ejemplo/Metodos.java
+++ src/conceptos_ejemplo/Metodos.java
@@ -13,6 +13,22 @@ public class Metodos {
     
     //esta clase tendrá todos los metodos con los que se trabaja
     //creamos el constructor de la clase
+    private float salario;
     
+    public Metodos() {
+        
+    }
+    public void vacaciones(float salario,int dias){
+        float monto=(salario/30)*(30-dias);
+        
+        System.out.println("EL monto ha pagar es de "+String.valueOf(monto));
+        
+    }
+    public void salario_dia(float salario){
+        //metodo para calcular el salario por dia
+        float dia=salario/30;
+        
+        System.out.println("EL monto es de "+String.valueOf(dia));
+    }
     
 }
--- src/conceptos_ejemplo/Trabajador.java
+++ src/conceptos_ejemplo/Trabajador.java
@@ -5,6 +5,7 @@
  */
 package conceptos_ejemplo;
 import java.util.Scanner;//esta clase nos servira para obtener datos del usuario
+import javax.swing.*;
 
 /**
  *
@@ -14,11 +15,31 @@ public class Trabajador extends Persona {//heredamos todos los atributos de la c
     private float Salario;
     private String ocupacion;
     
+    
+    //encapsulamos los atributos
+
+    public float getSalario() {
+        return Salario;
+    }
+
+    public void setSalario(float Salario) {
+        this.Salario = Salario;
+    }
+
+    public String getOcupacion() {
+        return ocupacion;
+    }
+
+    public void setOcupacion(String ocupacion) {
+        this.ocupacion = ocupacion;
+    }
+    
     public static void main(String []args){//creamos el main para trabajar
-        String dato,mostrar;
+        String dato;
         int i=0;
         Trabajador trabajador =new Trabajador();//creamos un arreglo de trabajadores
         Scanner entrada=new Scanner(System.in);//creamos el objeto de la clase scanner
+        Metodos metodos=new Metodos();
         
         //nombre
         System.out.println("inserta el nombre del trabajador");
@@ -33,6 +54,45 @@ public class Trabajador extends Persona {//heredamos todos los atributos de la c
         //edad
         System.out.println("inserta la edad ");
         dato=entrada.nextLine();
+        trabajador.setEdad(Integer.parseInt(dato));//mandamos la edad pero hacemos un parseo para la compatibilidad de datos
+        
+        //ocupacion
+        System.out.println("inserta la ocupacion del trabajador");
+        dato=entrada.nextLine();
+        trabajador.setOcupacion(dato);
+        
+        //salario
+        System.out.println("inserta el Salario del trabajador");
+        dato=entrada.nextLine();
+        trabajador.setSalario(Float.parseFloat(dato));//hacemos otro parseo de string a float   
+        
+        //----------------mostramos un menú sencillo
+        System.out.println("   Selecciona una opcion");
+        System.out.println("1-Calculo de salario por dia");
+        System.out.println("2-Calculo de indemnizacion");
+        
+        int seleccion=entrada.nextInt();
+        switch(seleccion){
+            case 1:
+                //calculo de salario por dia
+                
+                float salario=trabajador.getSalario();
+                trabajador.detalle(trabajador);
+                metodos.salario_dia(salario);
+                break;
+            case 2:
+                //aguinaldo
+                
+                System.out.println("Inserta los dias de vacaciones gozados");
+                int dias=entrada.nextInt();
+                salario=trabajador.getSalario();
+                trabajador.detalle(trabajador);
+                metodos.vacaciones(salario, dias);
+                break;
+            default:
+                //nada XD
+                     break;
+        }
         
         
         
@@ -40,7 +100,12 @@ public class Trabajador extends Persona {//heredamos todos los atributos de la c
         
         
         
+    }
+     void detalle(Trabajador trab){
         
+        System.out.println("Nombre : "+trab.getNombre()+"\nApellido :"+trab.getApellido());
+        System.out.println("Ocupacion :"+trab.getOcupacion());
+        System.out.println("Salario Mensual:"+trab.getSalario());
         
     }
     
